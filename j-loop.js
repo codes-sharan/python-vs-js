3bd782a6043ad9dc2dfc465d8421cd5a71977467
Sharan Panthi <shpanthi09@gmail.com>
2024-10-21 15:39:25 +0545

    first commit

---
+++ j-loop.js
@@ -0,0 +1,11 @@
+// for loop
+for (let i = 0; i < 5; i++) {
+  console.log("Iteration:", i);
+}
+
+// while loop
+let count = 0;
+while (count < 5) {
+  console.log("Count:", count);
+  count++;
+}
---
+++ p-loop.py
@@ -0,0 +1,10 @@
+# for loop
+for i in range(5):
+    print("Iteration:", i)
+
+
+# while loop (cannot use count++ like in js)
+count = 0
+while count < 5:
+    print("Count:", count)
+    count += 1
